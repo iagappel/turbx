56c2509d551eb26b46fa063be1dfcd22462e71df
Jason Appelbaum <appelbaum@iag.uni-stuttgart.de>
2022-06-30 13:47:59 +0200

    pypi friendly structure

---
+++ setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import sys
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='turbx',
+    version='0.1.1',
+    description='Tools for analysis of turbulent flow datasets.',
+    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    
+    url='https://github.com/iagappel/turbx',
+    author='Jason Appelbaum',
+    maintainer='Jason Appelbaum',
+    #author_email='jason.appelbaum@icloud.com',
+    license='MIT',
+    packages=['turbx'],
+    #packages=find_packages(exclude=("tests",)),
+    install_requires=['mpi4py>=3.1',
+                      'numpy>=1.22',
+                      'scipy>=1.8',
+                      'h5py>=3.6',
+                      'matplotlib>=3.5',
+                      'scikit-image>=0.19',
+                      'psutil>=5.9',
+                      'tqdm>=4.64',
+                      'cmocean>=2.0',
+                      'colorcet>=3.0',
+                      'cmasher>=1.6',
+                      ],
+    
+    #setup_requires=['pytest-runner'],
+    python_requires='>=3.6',
+    #tests_require=['pytest'],
+    platforms=['any'],
+    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+    
+    keywords=['scientific computing', 'statistics', 'simulation', 'turbulence', 'turbulent flows', 'direct numerical simulation', 'DNS', 'parallel', 'visualization'],
+)
---
+++ turbx/__init__.py
@@ -0,0 +1,73 @@
+'''
+turbx
+
+Tools for analysis of turbulent flow datasets.
+'''
+
+__version__ = '0.1.1'
+__author__ = 'Jason Appelbaum'
+
+from .turbx import rgd
+from .turbx import cgd
+from .turbx import eas4
+from .turbx import lpd
+from .turbx import eas3
+
+from .turbx import curve_fitter
+
+from .turbx import fd_coeff_calculator
+from .turbx import gradient
+
+from .turbx import get_grad
+from .turbx import get_curl
+from .turbx import get_overlapping_window_size
+from .turbx import get_overlapping_windows
+from .turbx import ccor
+from .turbx import ccor_naive
+
+from .turbx import gulp
+
+from .turbx import format_time_string
+from .turbx import even_print
+
+from .turbx import set_mpl_env
+from .turbx import get_color_list
+from .turbx import get_Lch_colors
+from .turbx import hex2rgb
+from .turbx import hsv_adjust_hex
+from .turbx import cmap_hsv_adjust
+from .turbx import analytical_u_plus_y_plus
+from .turbx import fig_trim
+from .turbx import axs_grid_compress
+from .turbx import tight_layout_helper_ax_with_cbar
+from .turbx import cmap_convert_mpl_to_pview
+
+__all__ = ['rgd',
+           'cgd',
+           'eas4',
+           'lpd',
+           'eas3',
+           'curve_fitter',
+           'fd_coeff_calculator',
+           'gradient',
+           'get_grad',
+           'get_curl',
+           'get_overlapping_window_size',
+           'get_overlapping_windows',
+           'ccor',
+           'ccor_naive',
+           'gulp',
+           'format_time_string',
+           'even_print',
+           'set_mpl_env',
+           'get_color_list',
+           'get_Lch_colors',
+           'hex2rgb',
+           'hsv_adjust_hex',
+           'cmap_hsv_adjust',
+           'analytical_u_plus_y_plus',
+           'fig_trim',
+           'axs_grid_compress',
+           'tight_layout_helper_ax_with_cbar',
+           'cmap_convert_mpl_to_pview',
+           ]
